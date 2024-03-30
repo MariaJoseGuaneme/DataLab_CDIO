65346dcb7548fb05ca383108523b319618cbdb9a
Maria José Guaneme <mariaj.guanemeb@uqvirtual.edu.co>
2024-03-30 16:23:36 -0500

    corrección

--- Codigo_proyecto/base/lib/main.dart
+++ Codigo_proyecto/base/lib/main.dart
@@ -1,73 +1,106 @@
 import 'package:flutter/material.dart';
 
-void main (){
-runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ super.key });
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: PagInicio(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class PagInicio extends StatefulWidget {
-  const PagInicio({ super.key });
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+ 
+
+  final String title;
 
   @override
-  State<PagInicio> createState() => _PagInicio();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _PagInicio extends State<PagInicio> {
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+     
+      _counter++;
+    });
+  }
+
   @override
-  Widget build(BuildContext context) { //appBar
+  Widget build(BuildContext context) {
+
     return Scaffold(
-     backgroundColor: Colors.white,
-     body: inicio(),
-);
+      appBar: AppBar(
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
-}
-
-Widget inicio(){
-return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(
-              height:400 ,
-              width: 400,
-              child: Image.asset('assets/logou.jpg'), 
-            ),
-            const SizedBox(height: 20.0,),
-          Container(
-            width: 400,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.green,
-              border: Border.all(color: Colors.green,width: 4.0),
-              borderRadius: BorderRadius.circular(10.0)
-            ),
-
-            child: const Center(
-            child: Text('¡Bienvenido al sistema de recolección de datos!',
-             style: TextStyle(color: Colors.white,fontSize: 30,
-              fontWeight: FontWeight.bold,backgroundColor:Colors.green),
-            textAlign: TextAlign.center,)
-            ),
-            ),
-
-            const SizedBox(height: 20.0,),
-           ElevatedButton(
-            onPressed: (){}, 
-            style: ButtonStyle(backgroundColor:MaterialStateProperty.all(Colors.black),
-             shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)) ),),
-            child: const Text('Iniciar', style: TextStyle(color: Colors.white, fontSize: 30)),
-            ) ,
-            
-        ],
-      )
-   );}
