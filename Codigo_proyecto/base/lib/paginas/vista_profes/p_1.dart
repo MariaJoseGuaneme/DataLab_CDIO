ce271c50c439677b13d7cf63520f5c77d5855d02
Maria José Guaneme <mariaj.guanemeb@uqvirtual.edu.co>
2024-03-30 13:15:47 -0500

    Subo la organización para el desarrollo del proyecto.

---
+++ Codigo_proyecto/base/lib/paginas/vista_profes/p_1.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main (){
+runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PagInicio(),
+    );
+  }
+}
+
+class PagInicio extends StatefulWidget {
+  const PagInicio({ super.key });
+
+  @override
+  State<PagInicio> createState() => _PagInicio();
+}
+
+class _PagInicio extends State<PagInicio> {
+  @override
+  Widget build(BuildContext context) { //appBar
+    return Scaffold(
+     backgroundColor: Colors.white,
+     body: Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SizedBox(
+              height:400 ,
+              width: 400,
+              child: Image.asset('assets/logou.jpg'),
+            ),
+            SizedBox(height: 20.0,),
+          Container(
+            width: 400,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              border: Border.all(color: Colors.green,width: 4.0),
+              borderRadius: BorderRadius.circular(10.0)
+            ),
+
+            child: Center(
+            child: Text('¡Bienvenido al sistema de recolección de datos!',
+             style: TextStyle(color: Colors.white,fontSize: 30,
+              fontWeight: FontWeight.bold,backgroundColor:Colors.green),
+            textAlign: TextAlign.center,)
+            ),
+            ),
+
+            SizedBox(height: 20.0,),
+           ElevatedButton(
+            onPressed: (){}, 
+            style: ButtonStyle(backgroundColor:MaterialStateProperty.all(Colors.black),
+             shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)) ),),
+            child: Text('Iniciar', style: TextStyle(color: Colors.white, fontSize: 30)),
+            ) ,
+            
+        ],
+      )
+   )
+);
+}
+}
