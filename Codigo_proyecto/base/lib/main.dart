f7e2e9adee2c2a2e2ef8f42c53b7d51023089481
Maria José Guaneme <mariaj.guanemeb@uqvirtual.edu.co>
2024-03-31 19:37:57 -0500

    cambio 03-31

--- Codigo_proyecto/base/lib/main.dart
+++ Codigo_proyecto/base/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: PagInicio(),
+      home: PagInicio(), //Acá cambian por el widget que ustedes creen
     );
   }
 }
