53d59be6e154417e30ac21bf6175b50e8c65ae1e
Maria José Guaneme <mariaj.guanemeb@uqvirtual.edu.co>
2024-04-23 20:52:27 -0500

    perez2

--- Codigo_proyecto/base/lib/main.dart
+++ Codigo_proyecto/base/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:base/funciones_proyecto/select_actividad_estudiante.dart';
 import 'package:base/funciones_proyecto/select_actividad_profe.dart';
 import 'package:base/funciones_proyecto/select_proceso.dart';
 import 'package:base/studentoperationspage.dart';
+import 'package:base/paginas/vista_profes/p_10.dart';
 
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       //home: PagInicio(),
       //home: Select_e(),
-      home: Select_p(),
+      //home: Select_p(),
+      home: PagInicio10(),
     );
   }
 }
